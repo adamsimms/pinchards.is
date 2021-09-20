1c653426d01f60b5f819a8f2ed449bedf55a0ff4
Adam Simms <hello@adamsim.ms>
2021-09-20 11:13:12 -0400

    Update getphotos.php

--- getphotos.php
+++ getphotos.php
@@ -2,7 +2,7 @@
 
 //require_once('vender/aws/S3.php');
 //require 'vendor/aws/S3.php';
-//$s3 = new S3('REDACTED_AWS_ACCESS_KEY_ID', 'REDACTED_AWS_SECRET_ACCESS_KEY');
+//$s3 = new S3('KEY', 'SECRET');
 // 
 //$bucket_contents = $s3->getBucket('shutter-island');
 //foreach ($bucket_contents as $file){
